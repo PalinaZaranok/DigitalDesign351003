3bb908668f783fccfa8fed846ff34d830744d928
Vitali Zhuravski <vitaliycof@outlook.com>
2026-02-18 17:13:12 +0300

    Обновил Task7.

--- vitali_zhuravski/1/rtl/Tsk7Top.vhd
+++ vitali_zhuravski/1/rtl/Tsk7Top.vhd
@@ -1,13 +1,16 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-library UNISIM;
-use UNISIM.VComponents.all;
-
 -- 3: nAnBCD
 -- 2: nAnBCnD
 -- 1: nAnBnCD
 -- 0: nAnBnCnD
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity tsk7_top is
     port (
@@ -26,24 +29,41 @@ architecture structure of tsk7_top is
     signal nB : std_logic;
     signal nC : std_logic;
     signal nD : std_logic;
+    signal GND : std_logic;
     
     signal nAnB : std_logic;
     signal nAnBC : std_logic;
     signal nAnBnC : std_logic;
+    
+    component MUX2
+        port(
+            I0, I1, S : in std_logic;
+            O : out std_logic
+        );
+    end component;
+    
 begin
     led_out(15 downto 4) <= "000000000000";
+    
+    GND <= '0';
     
     U0 : INV port map (I => A, O => nA);
     U1 : INV port map (I => B, O => nB);
     U2 : INV port map (I => C, O => nC);
     U3 : INV port map (I => D, O => nD);
     
-    U4 : AND2 port map (I0 => nA, I1 => nB, O => nAnB);
-    U5 : AND2 port map (I0 => nAnB, I1 => C, O => nAnBC);
-    U6 : AND2 port map (I0 => nAnB, I1 => nC, O => nAnBnC);
-    
-    U7 : AND2 port map (I0 => nAnBC, I1 => D, O => led_out(3));
-    U8 : AND2 port map (I0 => nAnBC, I1 => nD, O => led_out(2));
-    U9 : AND2 port map (I0 => nAnBnC, I1 => D, O => led_out(1));
-    U10 : AND2 port map (I0 => nAnBnC, I1 => nD, O => led_out(0));
+    U4 : MUX2 port map (I0 => GND, I1 => nA, S => nB, O => nAnB);
+    U5 : MUX2 port map (I0 => GND, I1 => nAnB, S => C, O => nAnBC);
+    U6 : MUX2 port map (I0 => GND, I1 => nAnB, S => nC, O => nAnBnC);
+    U7 : MUX2 port map (I0 => GND, I1 => nAnBC, S => D, O => led_out(3));
+    U8 : MUX2 port map (I0 => GND, I1 => nAnBC, S => nD, O => led_out(2));
+    U9 : MUX2 port map (I0 => GND, I1 => nAnBnC, S => D, O => led_out(1));
+    U10 : MUX2 port map (I0 => GND, I1 => nAnBnC, S => nD, O => led_out(0));
+    -- U4 : AND2 port map (I0 => nA, I1 => nB, O => nAnB);
+    -- U5 : AND2 port map (I0 => nAnB, I1 => C, O => nAnBC);
+    -- U6 : AND2 port map (I0 => nAnB, I1 => nC, O => nAnBnC);
+    -- U7 : AND2 port map (I0 => nAnBC, I1 => D, O => led_out(3));
+    -- U8 : AND2 port map (I0 => nAnBC, I1 => nD, O => led_out(2));
+    -- U9 : AND2 port map (I0 => nAnBnC, I1 => D, O => led_out(1));
+    -- U10 : AND2 port map (I0 => nAnBnC, I1 => nD, O => led_out(0));
 end structure;
